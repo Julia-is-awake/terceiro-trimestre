b0d321a3e8efb1bde1230841f0b66e57b0d8913b
julia-is-awake <juliasorgetzalves@gmail.com>
2024-09-04 08:45:44 -0300

    primeiro commit

--- src/aula31/classeAbstrata/Circulo.java
+++ src/aula31/classeAbstrata/Circulo.java
@@ -1,2 +1,27 @@
-package aula31.classeAbstrata;public class Circulo {
+package aula31.classeAbstrata;
+
+public class Circulo extends Figura{
+    private double raio;
+
+    public Circulo(){
+        setNomeFigura("Circulo");
+    }
+
+    public double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(double raio) {
+        this.raio = raio;
+    }
+
+    @Override
+    public double calculaArea(){
+        return Math.PI*Math.pow(raio, 2);
+    }
+
+    @Override
+    public double calculaPerimetro(){
+        return 2*Math.PI*raio;
+    }
 }
--- src/aula31/classeAbstrata/Figura.java
+++ src/aula31/classeAbstrata/Figura.java
@@ -1,2 +1,24 @@
-package aula31.classeAbstrata;public class Figura {
+package aula31.classeAbstrata;
+
+public abstract class Figura {
+    public String nomeFigura;
+
+    public String getNomeFigura(){
+        return nomeFigura;
+    }
+
+    public void setNomeFigura(String nomeFigura) {
+        this.nomeFigura = nomeFigura;
+    }
+
+    public abstract double calculaArea();
+
+    public abstract double calculaPerimetro();
+
+    public String toStrig(){
+        String retorno = "Nome: "+nomeFigura;
+        retorno+="\nÁrea: " +calculaArea();
+        retorno+="\nPerímetro: "+calculaPerimetro();
+        return retorno;
+    }
 }
